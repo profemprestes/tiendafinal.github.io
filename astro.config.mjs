a7964bb355053d402cabc8d9c10cfd47e34edf50
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-03-31 02:37:43 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import icon from "astro-icon";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(), 
-    icon({
-      include: {
-        tabler: ["*"] // Include all icons from the tabler icon set
-      }
-    }), 
     react()
   ],
   site: 'https://profemprestes.github.io/tiendafinal',

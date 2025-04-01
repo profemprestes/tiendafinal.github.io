16df8e732b4db4364791e96e3abfde1f9cd04074
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-01 19:27:04 -0300

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -25,6 +23,6 @@ export default defineConfig({
   compressHTML: true,
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: []
   }
 });
